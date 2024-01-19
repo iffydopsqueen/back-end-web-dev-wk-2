94e049506ea9a7b012ecb141b201bc8f98bfdf68
Iffy Eleweke <omaleweks@gmail.com>
2024-01-19 00:07:59 -0500

    Added HTTP authentication

--- Week 2/week-2-day-3/m3-w2-lab-assignment/demo-node-app/routes/index.js
+++ Week 2/week-2-day-3/m3-w2-lab-assignment/demo-node-app/routes/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+const auth = require('http-auth');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 const Registration = mongoose.model('Registration');
 
+const basic = auth.basic({
+    file: path.join(__dirname, '../users.htpasswd'),
+});
+
 router.get('/', function(req, res) {
     res.render('form', { title: 'Registration form' });
 });
 
-router.get('/registrations', (req, res) => {
+router.get('/registrations', basic.check((req, res) => {
     Registration.find()
         .then((registrations) => {
             res.render('index', { title: 'Listing registrations' });
         })
         .catch(() => { res.send('Sorry! Something went wrong.'); });
-});
+}));
 
 router.post('/', 
     [
