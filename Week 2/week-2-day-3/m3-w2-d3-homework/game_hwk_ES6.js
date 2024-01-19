9e40b1f0c58b3e35f88d4280f10b5855a47ecdff
Iffy Eleweke <omaleweks@gmail.com>
2024-01-19 13:51:22 -0500

    Converting the functions and variable declarations to ES6 - Part I

--- Week 2/week-2-day-3/m3-w2-d3-homework/game_hwk_ES6.js
+++ Week 2/week-2-day-3/m3-w2-d3-homework/game_hwk_ES6.js
@@ -1,1 +1,31 @@
 // ES6 conversions 
+
+//on page load -> generate game board;
+window.onload = () => {
+    console.log("Page Loaded");
+    setRandomTileOrder(12);
+    setTiles();
+}
+
+//global variable
+let i = 0;
+let clicks;
+let timeScore;
+
+/*start button initiates game and starts counter
+initiates game start on button press*/
+const startButton = document.getElementById("startGame");
+startButton.addEventListener("click", startGame);
+
+const startGame = () => {
+    tiles.forEach(tile => tile.addEventListener("click", displayTile));
+    resetTiles();
+    startButton.disabled = true;
+     console.log(randomOrderArray);
+    startTimer();
+}
+
+
+
+
+
